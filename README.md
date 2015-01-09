892ca6e3aa36ff0f49a19f8d7f0188de5eb9bf2f
Stephen Brennan <smb196@case.edu>
2015-01-08 23:16:34 -0500

    Initial commit: added a prototype, incredibly naive shell.

---
+++ README.md
@@ -0,0 +1,13 @@
+LSH
+===
+
+LSH is my little attempt at a *real* shell in C.  It's called LSH because it's
+supposed to be the Libstephen SHell.  That's because I hope that if this shell
+becomes anything, it could become a part of Libstephen as a shell library.  It
+would have a simple shell syntax, maybe optional globbing, and it would be
+simple to plug any set of commands into it.  That way, it could be used for
+quick interfaces by defining simple commands.
+
+But right now, it's just a simple shell to demonstrate that I know how a shell
+works.  That is, read, parse, fork, exec, and wait.  So I think it's pretty
+cool.
---
+++ src/main.c
@@ -0,0 +1,178 @@
+/***************************************************************************//**
+
+  @file         main.c
+
+  @author       Stephen Brennan
+
+  @date         Thursday,  8 January 2015
+
+  @brief        LSH (Libstephen SHell)
+
+*******************************************************************************/
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return 0
+ */
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execv(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 0;
+}
+
+#define LSH_RL_BUFSIZE 1024
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE;
+  char **tokens = malloc(bufsize * sizeof(char));
+  int position = 0;
+  char *token;
+  int toklen;
+  char *token_copy;
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    toklen = strlen(token);
+    token_copy = malloc((toklen + 1) * sizeof(char));
+    strcpy(token_copy, token);
+
+    tokens[position] = token_copy;
+    position++;
+
+    if (position <= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize);
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+/**
+   @brief Free null-terminated array of strings.
+   @param args List to free.
+ */
+void lsh_free_args(char **args)
+{
+  char **iter = args;
+  while (*iter != NULL) {
+    free(*iter);
+    iter++;
+  }
+  free(args);
+}
+
+/**
+   @brief Loop getting input and executing it.
+ */
+void lsh_loop(void)
+{
+  char *line;
+  char **args;
+  int status = 1;
+
+  do {
+    printf("> ");
+    char *line = lsh_read_line();
+    char **args = lsh_split_line(line);
+    free(line);
+    lsh_launch(args);
+    lsh_free_args(args);
+  } while (status);
+}
+
+/**
+   @brief Main entry point.
+   @param argc Argument count.
+   @param argv Argument vector.
+   @return status code
+ */
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  lsh_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return 0;
+}
+
