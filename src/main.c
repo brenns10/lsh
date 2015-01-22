24d6d421e7a1658a410d349337a3dbf88b3db664
Joseph <josephok@qq.com>
2015-01-22 11:36:33 +0800

    add error checks when fork

--- src/main.c
+++ src/main.c
@@ -108,11 +108,13 @@ int lsh_launch(char **args)
     // Child process
     if (execvp(args[0], args) == -1) {
       perror("lsh");
+      exit(EXIT_FAILURE);
     }
-    exit(EXIT_FAILURE);
+    exit(EXIT_SUCCESS);
   } else if (pid < 0) {
     // Error forking
     perror("lsh");
+    exit(EXIT_FAILURE);
   } else {
     // Parent process
     do {
