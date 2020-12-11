b2e99072c77cc740d8131685aa544a8c1fbb1273
Tim Gates <tim.gates@iress.com>
2020-12-11 17:50:10 +1100

    docs: fix simple typo, recieved -> received
    There is a small typo in src/main.c.
    
    Should read `received` rather than `recieved`.

--- src/main.c
+++ src/main.c
@@ -158,7 +158,7 @@ char *lsh_read_line(void)
   ssize_t bufsize = 0; // have getline allocate a buffer for us
   if (getline(&line, &bufsize, stdin) == -1) {
     if (feof(stdin)) {
-      exit(EXIT_SUCCESS);  // We recieved an EOF
+      exit(EXIT_SUCCESS);  // We received an EOF
     } else  {
       perror("lsh: getline\n");
       exit(EXIT_FAILURE);
